2f88069294236975bc292f9761e1667e2a39954c
wizardchiboy <doncodesacademy@gmail.com>
2022-09-02 22:42:20 +0100

    day 2 - worked on header layer

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,6 +7,13 @@ import {
   JumiaPrime,
   StarLogo,
 } from "../imports/Imports";
+import {
+  ArrowDropDownIcon,
+  HelpOutlineIcon,
+  PersonIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "../material-icons/Icons";
 import "./Header.css";
 
 function Header() {
@@ -27,6 +34,41 @@ function Header() {
             <img src={JumiaPay} alt="jumia pay" />
             <img src={JumiaFood} alt="jumia food" />
             <img src={JumiaPrime} alt="jumia prime" />
+          </div>
+        </div>
+
+        <div className="header__layer__three">
+          <div className="header__layer__three__logo">
+            <img src={JumiaLogo} alt="jumia logo" />
+          </div>
+
+          <div className="header__layer__three__cta">
+            <div className="header__layer__three__cta__search">
+              <SearchIcon />
+              <input
+                type="search"
+                placeholder="search products, brands and categories"
+                id=""
+              />
+            </div>
+            <div className="header__layer__three__cta__search__btn">search</div>
+
+            <div className="header__layer__three__cta__account">
+              <PersonIcon />
+              <p>account</p>
+              <ArrowDropDownIcon />
+            </div>
+
+            <div className="header__layer__three__cta__help">
+              <HelpOutlineIcon />
+              <p>help</p>
+              <ArrowDropDownIcon />
+            </div>
+
+            <div className="header__layer__three__cta__cart">
+              <ShoppingCartIcon />
+              <p>cart</p>
+            </div>
           </div>
         </div>
       </div>
---
+++ src/components/material-icons/Icons.js
@@ -0,0 +1,13 @@
+import SearchIcon from "@mui/icons-material/Search";
+import PersonIcon from "@mui/icons-material/Person";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+
+export {
+  SearchIcon,
+  PersonIcon,
+  ArrowDropDownIcon,
+  ShoppingCartIcon,
+  HelpOutlineIcon,
+};
